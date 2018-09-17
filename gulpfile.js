ad379d8f242af4eb60500eae1f6bedaabae71f29
BairachnyiVadym <coworking1b.v@gmail.com>
2018-09-17 13:30:58 +0300

    Install node.js, sass, gulp. Configure gulpfile.js: livereload, autoprefixer, compile and minify sass.

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+let gulp = require('gulp');
+let livereload = require('gulp-livereload');
+let sass = require('gulp-sass');
+let autoprefixer = require('gulp-autoprefixer');
+let rename = require('gulp-rename');
+let cleancss = require('gulp-clean-css');
+
+gulp.task('reload-css', function () {
+    gulp.src('./src/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 3 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./css/'))
+        .pipe(cleancss({compatibility: 'ie8'}))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./css/'))
+        .pipe(livereload());
+});
+
+gulp.task('default', function() {
+    livereload.listen();
+    gulp.watch('./src/*.scss', ['reload-css']);
+});
