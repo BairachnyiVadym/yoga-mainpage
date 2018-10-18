b125c69a1b14b2e49d554beab8473286d6d9dade
BairachnyiVadym <coworking1b.v@gmail.com>
2018-10-18 14:49:42 +0300

    add phone validation

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,6 +1,7 @@
 let submitButton = document.getElementById("slider-form-submit");
 
 submitButton.addEventListener("click", nameValidationHandler);
+submitButton.addEventListener("click", phoneValidationHandler);
 
 function nameValidationHandler() {
     let fld = document.getElementsByName("name")[0];
@@ -25,6 +26,34 @@ function nameValidationHandler() {
         alert(error);
         return false;
 
+    } else {
+        fld.style.background = "#F2F2F2";
+    }
+    return true;
+}
+
+function phoneValidationHandler() {
+    let fld = document.getElementsByName("phone")[0];
+    let error = "";
+    let stripped = fld.value.replace(/[\(\)\.\-\ ]/g, '');
+
+    if (fld.value == "") {
+        error = "You didn't enter a phone number.\n";
+        fld.style.background = 'Yellow';
+        alert(error);
+        return false;
+
+    } else if (isNaN(parseInt(stripped))) {
+        error = "The phone number contains illegal characters. Don't include dash (-)\n";
+        fld.style.background = 'Yellow';
+        alert(error);
+        return false;
+    } else if (!(stripped.length == 12)) {
+        error = "The phone number is the wrong length. Make sure you included an area code. " +
+            "Don't include plus sign('+') and parentheses ('()')\n";
+        fld.style.background = 'Yellow';
+        alert(error);
+        return false;
     } else {
         fld.style.background = "#F2F2F2";
     }
