e9b730b01dbde9a2d6574d36fd702d42635185c2
BairachnyiVadym <coworking1b.v@gmail.com>
2018-11-01 13:28:44 +0200

    add correction of scrollTop value, attach event listeners to nav links to control hamburger menu flow

--- js/scrollToSection.js
+++ js/scrollToSection.js
@@ -1,11 +1,14 @@
 jQuery(document).ready(function($) {
+    let headerHeight = $(".header").css('height');
+    headerHeight = parseInt(headerHeight, 10);
+
     function scrollToSection(event) {
         event.preventDefault();
 
         let $section = $($(this).attr('href'));
 
         $('html, body').animate({
-            scrollTop: $section.offset().top -100 // -100px according to header height
+            scrollTop: $section.offset().top -headerHeight
         }, 500);
     }
     $('[data-scroll]').on('click', scrollToSection);
--- js/toggleMenu.js
+++ js/toggleMenu.js
@@ -3,9 +3,19 @@ let menuButton = document.getElementById("menu-btn");
 menuButton.addEventListener("click", toggleMenuHandler);
 menuButton.addEventListener("click", layoutOrderHandler);
 
-function toggleMenuHandler() {
-    let mainMenu = document.querySelectorAll('.main-menu');
+let mainMenu = document.querySelectorAll('.main-menu');
+let menuLinks = document.querySelectorAll('.links');
 
+for (let i = 0; i < menuLinks.length; i++) {
+    menuLinks[i].onclick = function () {
+        mainMenu[0].style.maxHeight = "";
+        mainMenu[1].style.maxHeight = "";
+        menuButton.checked = false;
+        layoutOrderHandler();
+    }
+}
+
+function toggleMenuHandler() {
     for(let i = 0; i < mainMenu.length; i++) {
         if(menuButton.checked) {
             mainMenu[i].style.maxHeight = "240px";
@@ -25,6 +35,3 @@ function layoutOrderHandler() {
         mobHeader.parentNode.insertBefore(mainMenuLeft, mobHeader);
     }
 }
-
-
-
